662edc53d44a30dd4ade010c1b623995971ae652
Mugiwawa <adrien.fonnet@gmail.com>
2021-06-12 01:02:29 +0200

    Basic Camera Follow

--- GMTK Game Jam/Assets/Scripts/CameraManager.cs
+++ GMTK Game Jam/Assets/Scripts/CameraManager.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField] float offsetX;
+    [SerializeField] float offsetY;
+    
+    float cameraSize;
+    [SerializeField] Player player;
+    //[SerializeField] Soul soul;
+
+    private void Start()
+    {
+        cameraSize = GetComponent<Camera>().orthographicSize;
+    }
+
+    private void Update()
+    {
+        CenterCamera();
+    }
+
     public void CenterCamera()
     {
-
+        //if(player.isLinked)
+        transform.position = player.transform.position + new Vector3(offsetX, offsetY, -1);
     }
 }

e36f09843a37771b8520dbe796e658343e5f41cf
HuMoreau <h.moreau.git@gmail.com>
2021-06-12 02:43:27 +0200

    Déplacement et binding/débinding de la soul

--- GMTK Game Jam/Assets/Scripts/Entities/Player.cs
+++ GMTK Game Jam/Assets/Scripts/Entities/Player.cs
@@ -5,11 +5,68 @@ using UnityEngine;
 public class Player : Entity, IAttackable
 {
     public int Hp { get; set; }
+    public bool debug = false;
+    public bool isCombined = true;
+    public Soul soul;
+    public Collider2D soulCollider;
+    private bool isSoulInRange = true;
+    public SpriteRenderer spriteRenderer;
+    public float timerBeforeCombineAgain = 0f;
+    
+    void Update()
+    {
+        if(timerBeforeCombineAgain > 0)
+        {
+            timerBeforeCombineAgain -= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isCombined && timerBeforeCombineAgain <= 0)
+            {
+                isCombined = false;
+                timerBeforeCombineAgain = 2f;
+            }
+
+            if (!isCombined && timerBeforeCombineAgain <= 0)
+            {
+                isCombined = true;
+                timerBeforeCombineAgain = 2f;
+            }
+        }
+
+
+        if(isSoulInRange == false && debug == true)
+        {
+            spriteRenderer.color = Color.green;
+        }
+
+        if(isSoulInRange == true && debug == true)
+        {
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "Soul")
+        {
+            isSoulInRange = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D col) 
+    {
+        if (col.name == "Soul")
+        {
+            isSoulInRange = false;
+        }
+    }
 
     public void OnAttacked()
     {
         throw new System.NotImplementedException();
     }
-
 
 }

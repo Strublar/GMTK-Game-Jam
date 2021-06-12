2470db230b0c9fe7e0720564606ced4ff8e0365d
Mugiwawa <adrien.fonnet@gmail.com>
2021-06-12 08:30:54 +0200

    Add intro and start of gamemanager

---
+++ GMTK Game Jam/Assets/Scripts/UI/DialogBox.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogBox : MonoBehaviour
+{
+    [SerializeField] GameObject textBox;
+    [SerializeField] TextMeshProUGUI textMesh;
+    int lettersPerSecond = 30;
+
+    public IEnumerator TypeDialog(string dialog, float delayInSec = 0f)
+    {
+        textBox.SetActive(true);
+        textMesh.text = "";
+        foreach (char letter in dialog.ToCharArray())
+        {
+            textMesh.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+
+        yield return new WaitForSeconds(delayInSec);
+    }
+}

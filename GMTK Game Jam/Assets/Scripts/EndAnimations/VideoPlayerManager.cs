e3c173a2e63b8aa057cd606a6c4cd080311ea46a
Strublar <daluzmatteo@gmail.com>
2021-06-13 17:47:45 +0200

    fixed intro

---
+++ GMTK Game Jam/Assets/Scripts/EndAnimations/VideoPlayerManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerManager : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer;
+    public string fileName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, fileName+".avi");
+        videoPlayer.Play();
+    }
+
+
+}

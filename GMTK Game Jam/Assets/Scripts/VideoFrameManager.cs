a7629140f4ffe6198aa31fb0586dfdd3597167e4
Mugiwawa <adrien.fonnet@gmail.com>
2021-06-13 18:31:56 +0200

    code a video player to display frame

---
+++ GMTK Game Jam/Assets/Scripts/VideoFrameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoFrameManager : MonoBehaviour
+{
+    [SerializeField] Image videoScreen;
+    [SerializeField] List<Sprite> frames;
+    [SerializeField] float speed;
+
+    private void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    private IEnumerator PlayVideo()
+    {
+        foreach(Sprite frame in frames)
+        {
+            videoScreen.sprite = frame;
+            yield return new WaitForSeconds(1 / speed);
+        }
+    }
+
+
+}
